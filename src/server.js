7a4ee72259a13757075081d85e2b353306e6d905
Marcelo Velasco <marcelo.velasco@solvimm.com>
2021-03-07 13:05:22 -0300

    dockerhub integration

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();         
-const port = 3000; //porta padrão
+const PORT = 3000; //porta padrão
 const HOST = '0.0.0.0';
 const mysql = require('mysql');
 
